1c38f3f649ad4c0db674ac2695e93a88b64eb398
MedzikUser <medzik@duck.com>
2022-11-20 18:55:11 +0100

    fix: set api url to official host

--- var.go
+++ var.go
@@ -2,6 +2,6 @@ package micropass_api
 
 var (
 	PasswordIterations = 100000
-	// ApiUrl             = "https://micropass-api.medzik.xyz"
-	ApiUrl = "http://localhost:3000/api"
+	ApiUrl             = "https://micropass-api.medzik.xyz"
+	// ApiUrl = "http://localhost:3000/api"
 )
